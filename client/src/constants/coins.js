fb96bbfc936be5d94826e434f8b3910f6329a640
CoopTRUE <coopdog4545@gmail.com>
2022-05-29 21:18:04 -0700

    beginning to add support for fantom and avalanche

--- client/src/constants/coins.js
+++ client/src/constants/coins.js
@@ -5,6 +5,10 @@ export default {
         USDT: '0x55d398326f99059fF775485246999027B3197955'
     },
     250: {// Fantom Opera
-        USDC: '0x04068da6c83afcfa0e13ba15a6696662335d5b75'
+        USDC: '0x04068da6c83afcfa0e13ba15a6696662335d5b75',
+        fUSDT: '0x049d68029688eabf473097a2fc38ef61633a3c7a'
+    },
+    43114: {// Avalanche
+        'USDC.e': '0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664'
     }
 }
