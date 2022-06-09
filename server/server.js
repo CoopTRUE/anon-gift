1ab406426c7bb6f2e74a4a8dd481a3dfdc5841f0
CoopTRUE <coopdog4545@gmail.com>
2022-06-08 21:01:06 -0700

    got code api routing working

--- server/server.js
+++ server/server.js
@@ -4,21 +4,26 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
-app.use(cors());
 
 app.use(express.static(path.resolve(__dirname, "../client/dist")));
 
-app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
-});
-
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 const connection = mongoose.connection;
-connection.once('open', function() {
+connection.once('open', function () {
     console.log("MongoDB database connection established successfully!");
 })
 
+// api
+app.get('/getAvailable', cors(), async (req, res) => {
+    console.log(req.json);
+    return res.json( true );
+})
+
+
 const PORT = process.env.PORT || 5000;
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+});
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
 });
