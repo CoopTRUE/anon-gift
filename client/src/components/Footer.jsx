40ef12a16e74265ac5ab3a01bc95e79a39ee6f21
CoopTRUE <coopdog4545@gmail.com>
2022-06-08 17:54:18 -0700

    correctly imported images and added rickroll

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react'
 import styles from './Footer.module.css'
-
+import octocatUrl from '../assets/img/octocat.svg'
+import rickrollUrl from '../assets/img/rickroll.svg'
 export default function Footer() {
     const year = useMemo(() => {
         const date = new Date()
@@ -10,7 +11,8 @@ export default function Footer() {
     return (
         <footer className={styles.container}>
             <p className={styles.text}>© Anon Gift {year}. Made with ❤️ in SF</p>
-            <input className={styles.imageButton} onClick={() => window.location='https://github.com/CoopTRUE/anon-gift'} type='image' src='src/assets/octocat.svg'/>
+            <input className={styles.imageButton} onClick={() => window.location='https://github.com/CoopTRUE/anon-gift'} type='image' src={octocatUrl}/>
+            <input className={styles.imageButton} onClick={() => window.location='https://www.youtube.com/watch?v=dQw4w9WgXcQ'} type='image' src={rickrollUrl}/>
         </footer>
     )
 }
